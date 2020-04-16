a7622c3a45de4abe10fac15bd6722a07ffb306cb
Tom van den Berge <tom.vandenberge@drillster.com>
2020-04-16 22:04:27 +0200

    Added Application class, test, and other corrections

---
+++ src/main/java/sample/Application.java
@@ -0,0 +1,10 @@
+package sample;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

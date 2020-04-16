2345152670e8116cae203635dd9c3eac22f65153
Anil Kumar Katta <anilkumarkatta@gmail.com>
2020-04-16 19:42:51 +0530

    Initial Commit

---
+++ spring-authorization-server/src/main/java/org/springframework/security/auth/server/SpringAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.security.auth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ spring-authorization-server/src/test/java/org/springframework/security/auth/server/SpringAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.security.auth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e12e05c11643f6aa46419d249554672077c172ed
Tom van den Berge <tom.vandenberge@drillster.com>
2020-04-17 09:27:49 +0200

    Add Empty Boot sample Fixes #1

--- src/test/java/sample/ApplicationTest.java
+++ src/test/java/sample/ApplicationTest.java
@@ -6,7 +6,7 @@ import org.springframework.context.ApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(classes = Application.class)
+@SpringBootTest
 public class ApplicationTest {
 
     @Test

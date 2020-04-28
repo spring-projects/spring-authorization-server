0d04733fee5037f9355ceda267976e388250d7f2
Sauli Ketola <sauli.ketola@nitor.fi>
2020-04-28 20:42:37 +0300

    unit test

--- samples/boot/oauth2resourceserver/src/test/java/sample/ResourceControllerTest.java
+++ samples/boot/oauth2resourceserver/src/test/java/sample/ResourceControllerTest.java
@@ -16,13 +16,19 @@
 package sample;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.Instant;
+import java.util.HashMap;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -33,9 +39,31 @@ public class ResourceControllerTest {
 	private MockMvc mockMvc;
 
 	@Test
-	public void shouldReturnUnauthorized() throws Exception {
-		this.mockMvc.perform(get("/").header("Authorization", "Bearer TOKEN")).andDo(print())
-				.andExpect(status().isUnauthorized());
+	public void shouldReturnOkWithToken() throws Exception {
+		this.mockMvc.perform(get("/").header("Authorization", "Bearer TOKEN"))
+			.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void shouldReturnUnauthorizedWithoutToken() throws Exception {
+		this.mockMvc.perform(get("/"))
+			.andExpect(status().isUnauthorized());
 	}
 
+	@TestConfiguration
+	static class ResourceControllerTestConfiguration {
+		@Bean
+		public JwtDecoder jwtDecoder() {
+			return (token) -> {
+				HashMap<String, Object> headers = new HashMap<>();
+				headers.put("alg", "RS256");
+				headers.put("typ", "JWT");
+
+				HashMap<String, Object> claims = new HashMap<>();
+				claims.put("sub", "1234567");
+				claims.put("name", "John Doe");
+				return new Jwt(token, Instant.now(), Instant.now().plusMillis(5000), headers, claims);
+			};
+		}
+	}
 }

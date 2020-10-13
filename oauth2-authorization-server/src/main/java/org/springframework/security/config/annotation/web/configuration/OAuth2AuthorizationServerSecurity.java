45283b42b49542f27ee2ad7ae670a2ac9bd1cb92
Joe Grandja <jgrandja@vmware.com>
2020-10-13 14:32:25 -0400

    Expose default security configuration
    Closes gh-110

--- oauth2-authorization-server/src/main/java/org/springframework/security/config/annotation/web/configuration/OAuth2AuthorizationServerSecurity.java
+++ oauth2-authorization-server/src/main/java/org/springframework/security/config/annotation/web/configuration/OAuth2AuthorizationServerSecurity.java
@@ -33,9 +33,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class OAuth2AuthorizationServerSecurity extends WebSecurityConfigurerAdapter {
 
-	// @formatter:off
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		applyDefaultConfiguration(http);
+	}
+
+	// @formatter:off
+	public static void applyDefaultConfiguration(HttpSecurity http) throws Exception {
 		OAuth2AuthorizationServerConfigurer<HttpSecurity> authorizationServerConfigurer =
 				new OAuth2AuthorizationServerConfigurer<>();
 		RequestMatcher[] endpointMatchers = authorizationServerConfigurer

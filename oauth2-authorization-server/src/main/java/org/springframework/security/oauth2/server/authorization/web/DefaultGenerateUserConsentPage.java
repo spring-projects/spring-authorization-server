2d7889e6cd3f6253db11d029fc9f28b83c099411
shanhy <xz.xiaoshan@gmail.com>
2021-03-30 18:50:32 +0800

    add License

--- oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/server/authorization/web/DefaultGenerateUserConsentPage.java
+++ oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/server/authorization/web/DefaultGenerateUserConsentPage.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.security.oauth2.server.authorization.web;
 
 import org.springframework.http.MediaType;
@@ -20,7 +35,7 @@ import java.util.Set;
  * @author shanhy
  * @date 2021/3/30 18:07
  */
-public class DefaultGenerateUserConsentPage implements UserConsentPage{
+public class DefaultGenerateUserConsentPage implements UserConsentPage {
 
 	private static final MediaType TEXT_HTML_UTF8 = new MediaType("text", "html", StandardCharsets.UTF_8);
 

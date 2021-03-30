1ab309c6392e646c14ab06cb7e6f69d22865d8da
shanhy <xz.xiaoshan@gmail.com>
2021-03-30 19:06:36 +0800

    add java docs

--- oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/server/authorization/web/UserConsentPage.java
+++ oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/server/authorization/web/UserConsentPage.java
@@ -34,17 +34,47 @@ public interface UserConsentPage {
 	String CONSENT_ACTION_APPROVE = "approve";
 	String CONSENT_ACTION_CANCEL = "cancel";
 
+	/**
+	 * Output and display consent
+	 *
+	 * @param request
+	 * @param response
+	 * @param registeredClient
+	 * @param authorization
+	 *
+	 * @throws IOException
+	 */
 	void displayConsent(HttpServletRequest request, HttpServletResponse response,
 			RegisteredClient registeredClient, OAuth2Authorization authorization) throws IOException;
 
+	/**
+	 * Is Consent Approved
+	 *
+	 * @param request
+	 * @return
+	 */
 	default boolean isConsentApproved(HttpServletRequest request) {
 		return CONSENT_ACTION_APPROVE.equalsIgnoreCase(request.getParameter(CONSENT_ACTION_PARAMETER_NAME));
 	}
 
+	/**
+	 * Is Consent Cancelled
+	 *
+	 * @param request
+	 * @return
+	 */
 	default boolean isConsentCancelled(HttpServletRequest request) {
 		return CONSENT_ACTION_CANCEL.equalsIgnoreCase(request.getParameter(CONSENT_ACTION_PARAMETER_NAME));
 	}
 
+	/**
+	 * Generate Consent Page HTML Code
+	 *
+	 * @param request
+	 * @param registeredClient
+	 * @param authorization
+	 * @return HTML Code
+	 */
 	String generateConsentPage(HttpServletRequest request,
 			RegisteredClient registeredClient, OAuth2Authorization authorization);
 

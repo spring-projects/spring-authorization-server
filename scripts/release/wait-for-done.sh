57aadceb1768b08d3ca02317a2581ebf62625255
Joe Grandja <jgrandja@vmware.com>
2021-08-17 05:58:41 -0400

    Remove references to experimental

--- scripts/release/wait-for-done.sh
+++ scripts/release/wait-for-done.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 VERSION=$1
-until http -h --check-status --ignore-stdin https://repo1.maven.org/maven2/org/springframework/security/experimental/spring-security-oauth2-authorization-server/$VERSION/; do sleep 10; clear; done; spd-say "It is now uploaded"
+until http -h --check-status --ignore-stdin https://repo1.maven.org/maven2/org/springframework/security/spring-security-oauth2-authorization-server/$VERSION/; do sleep 10; clear; done; spd-say "It is now uploaded"

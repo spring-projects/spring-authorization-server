104d273ba5f653fc1fef14ca5c3079be1828cad6
Joe Grandja <jgrandja@vmware.com>
2022-01-21 01:43:02 -0500

    Revert "Add "How-to: Implement core services with JPA""
    This reverts commit 525eca63d247478b0f4bbc5aed2d998eef70512f.

--- docs/modules/guides/examples/src/main/resources/oauth2-authorization-consent-schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE authorizationConsent (
-    registeredClientId varchar(255) NOT NULL,
-    principalName varchar(255) NOT NULL,
-    authorities varchar(1000) NOT NULL,
-    PRIMARY KEY (registeredClientId, principalName)
-);

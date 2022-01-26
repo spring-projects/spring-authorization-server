3812c0e56e4786f685ac1216cd489e4690b6adf1
Joe Grandja <jgrandja@vmware.com>
2022-01-26 12:08:10 -0500

    Next Development Version

--- oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/core/Version.java
+++ oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/core/Version.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2021 the original author or authors.
+ * Copyright 2020-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,7 @@ package org.springframework.security.oauth2.core;
 public final class Version {
 	private static final int MAJOR = 0;
 	private static final int MINOR = 2;
-	private static final int PATCH = 2;
+	private static final int PATCH = 3;
 
 	/**
 	 * Global Serialization value for Spring Security Authorization Server classes.

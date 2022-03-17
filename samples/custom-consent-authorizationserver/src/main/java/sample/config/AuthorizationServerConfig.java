c3402b0b121fab7451e4f13f1bb6d4b2930ecc2e
Steve Riesenberg <sriesenberg@vmware.com>
2022-03-17 14:46:48 -0500

    Replaced auth-server with localhost

--- samples/custom-consent-authorizationserver/src/main/java/sample/config/AuthorizationServerConfig.java
+++ samples/custom-consent-authorizationserver/src/main/java/sample/config/AuthorizationServerConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2021 the original author or authors.
+ * Copyright 2020-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -103,7 +103,7 @@ public class AuthorizationServerConfig {
 
 	@Bean
 	public ProviderSettings providerSettings() {
-		return ProviderSettings.builder().issuer("http://auth-server:9000").build();
+		return ProviderSettings.builder().issuer("http://localhost:9000").build();
 	}
 
 	@Bean

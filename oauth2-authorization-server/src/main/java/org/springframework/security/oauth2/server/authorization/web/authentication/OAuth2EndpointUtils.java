707b1c2325699d87c1f92a4ea6b8f5b3e440ab40
Hccake <chengbohua@foxmail.com>
2022-09-27 19:07:01 +0800

    Modify the access modifier of OAuth2EndpointUtils to public

--- oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/server/authorization/web/authentication/OAuth2EndpointUtils.java
+++ oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/server/authorization/web/authentication/OAuth2EndpointUtils.java
@@ -35,13 +35,13 @@ import org.springframework.util.MultiValueMap;
  * @author Joe Grandja
  * @since 0.1.2
  */
-final class OAuth2EndpointUtils {
+public final class OAuth2EndpointUtils {
 	static final String ACCESS_TOKEN_REQUEST_ERROR_URI = "https://datatracker.ietf.org/doc/html/rfc6749#section-5.2";
 
 	private OAuth2EndpointUtils() {
 	}
 
-	static MultiValueMap<String, String> getParameters(HttpServletRequest request) {
+	public static MultiValueMap<String, String> getParameters(HttpServletRequest request) {
 		Map<String, String[]> parameterMap = request.getParameterMap();
 		MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>(parameterMap.size());
 		parameterMap.forEach((key, values) -> {
@@ -54,7 +54,7 @@ final class OAuth2EndpointUtils {
 		return parameters;
 	}
 
-	static Map<String, Object> getParametersIfMatchesAuthorizationCodeGrantRequest(HttpServletRequest request, String... exclusions) {
+	public static Map<String, Object> getParametersIfMatchesAuthorizationCodeGrantRequest(HttpServletRequest request, String... exclusions) {
 		if (!matchesAuthorizationCodeGrantRequest(request)) {
 			return Collections.emptyMap();
 		}
@@ -65,18 +65,18 @@ final class OAuth2EndpointUtils {
 		return parameters;
 	}
 
-	static boolean matchesAuthorizationCodeGrantRequest(HttpServletRequest request) {
+	public static boolean matchesAuthorizationCodeGrantRequest(HttpServletRequest request) {
 		return AuthorizationGrantType.AUTHORIZATION_CODE.getValue().equals(
 				request.getParameter(OAuth2ParameterNames.GRANT_TYPE)) &&
 				request.getParameter(OAuth2ParameterNames.CODE) != null;
 	}
 
-	static boolean matchesPkceTokenRequest(HttpServletRequest request) {
+	public static boolean matchesPkceTokenRequest(HttpServletRequest request) {
 		return matchesAuthorizationCodeGrantRequest(request) &&
 				request.getParameter(PkceParameterNames.CODE_VERIFIER) != null;
 	}
 
-	static void throwError(String errorCode, String parameterName, String errorUri) {
+	public static void throwError(String errorCode, String parameterName, String errorUri) {
 		OAuth2Error error = new OAuth2Error(errorCode, "OAuth 2.0 Parameter: " + parameterName, errorUri);
 		throw new OAuth2AuthenticationException(error);
 	}

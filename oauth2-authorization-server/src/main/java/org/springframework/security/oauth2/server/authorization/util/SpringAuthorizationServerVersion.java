917988134765d297ec1be022d4fc3201c01c09fc
Joe Grandja <jgrandja@vmware.com>
2022-11-21 14:53:40 -0500

    Next Development Version

--- oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/server/authorization/util/SpringAuthorizationServerVersion.java
+++ oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/server/authorization/util/SpringAuthorizationServerVersion.java
@@ -24,7 +24,7 @@ package org.springframework.security.oauth2.server.authorization.util;
 public final class SpringAuthorizationServerVersion {
 	private static final int MAJOR = 1;
 	private static final int MINOR = 0;
-	private static final int PATCH = 0;
+	private static final int PATCH = 1;
 
 	/**
 	 * Global Serialization value for Spring Authorization Server classes.

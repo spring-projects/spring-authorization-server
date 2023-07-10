ebaba6f2d9d57adfbfdd979199b19bd86171dffc
Rob Winch <rwinch@users.noreply.github.com>
2023-07-10 12:00:13 -0500

    Use package naming conventions of lowercase
    This ensures the samples work with the code chomp extension

--- docs/src/main/java/sample/gettingstarted/SecurityConfig.java
+++ docs/src/main/java/sample/gettingstarted/SecurityConfig.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package sample.gettingStarted;
+package sample.gettingstarted;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
--- docs/src/main/java/sample/sociallogin/SecurityConfig.java
+++ docs/src/main/java/sample/sociallogin/SecurityConfig.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package sample.socialLogin;
+package sample.sociallogin;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

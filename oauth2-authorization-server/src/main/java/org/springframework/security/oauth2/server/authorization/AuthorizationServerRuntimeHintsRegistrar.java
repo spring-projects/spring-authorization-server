0c4df7f419a7c441cd8595d050a3f045eceeab86
Josh Long <josh@joshlong.com>
2023-11-14 16:01:53 -0800

    Move AOT hints to main module
    Closes gh-1446

--- oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/server/authorization/AuthorizationServerRuntimeHintsRegistrar.java
+++ oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/server/authorization/AuthorizationServerRuntimeHintsRegistrar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2023 the original author or authors.
+ * Copyright 2020-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,27 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package sample.aot.hint;
+package org.springframework.security.oauth2.server.authorization;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-
-import org.thymeleaf.expression.Lists;
-import sample.web.AuthorizationConsentController;
-
-import org.springframework.aot.hint.BindingReflectionHintsRegistrar;
-import org.springframework.aot.hint.MemberCategory;
-import org.springframework.aot.hint.RuntimeHints;
-import org.springframework.aot.hint.RuntimeHintsRegistrar;
-import org.springframework.aot.hint.TypeReference;
+import org.springframework.aot.hint.*;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.jackson2.CoreJackson2Module;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.client.jackson2.OAuth2ClientJackson2Module;
 import org.springframework.security.oauth2.core.AbstractOAuth2Token;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
@@ -48,75 +35,78 @@ import org.springframework.security.oauth2.server.authorization.jackson2.OAuth2A
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.jackson2.WebServletJackson2Module;
+import org.springframework.util.ClassUtils;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
- * {@link RuntimeHintsRegistrar} that registers {@link RuntimeHints} required for the sample.
- * Statically registered via META-INF/spring/aot.factories.
+ * {@link RuntimeHintsRegistrar} that registers {@link RuntimeHints} required for the
+ * sample. Statically registered via META-INF/spring/aot.factories.
  *
  * @author Joe Grandja
+ * @author Josh Long
  * @since 1.2
  */
-class DemoAuthorizationServerRuntimeHints implements RuntimeHintsRegistrar {
+class AuthorizationServerRuntimeHintsRegistrar implements RuntimeHintsRegistrar {
+
 	private final BindingReflectionHintsRegistrar reflectionHintsRegistrar = new BindingReflectionHintsRegistrar();
 
 	@Override
 	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
 
-		// Thymeleaf
-		hints.reflection().registerTypes(
-				Arrays.asList(
-						TypeReference.of(AuthorizationConsentController.ScopeWithDescription.class),
-						TypeReference.of(Lists.class)
-				), builder ->
-						builder.withMembers(MemberCategory.DECLARED_FIELDS,
-								MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS)
-		);
-
-		// Collections -> UnmodifiableSet, UnmodifiableList, UnmodifiableMap, UnmodifiableRandomAccessList, etc.
-		hints.reflection().registerType(
-				Collections.class, MemberCategory.DECLARED_CLASSES);
+		// Collections -> UnmodifiableSet, UnmodifiableList, UnmodifiableMap,
+		// UnmodifiableRandomAccessList, etc.
+		hints.reflection().registerType(Collections.class, MemberCategory.DECLARED_CLASSES);
 
 		// HashSet
-		hints.reflection().registerType(
-				HashSet.class, MemberCategory.DECLARED_FIELDS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
-				MemberCategory.INVOKE_DECLARED_METHODS);
+		hints.reflection().registerType(HashSet.class, MemberCategory.DECLARED_FIELDS,
+				MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS);
 
 		// Spring Security and Spring Authorization Server
-		hints.reflection().registerTypes(
-				Arrays.asList(
-						TypeReference.of(AbstractAuthenticationToken.class),
-						TypeReference.of(WebAuthenticationDetails.class),
-						TypeReference.of(UsernamePasswordAuthenticationToken.class),
-						TypeReference.of(User.class),
-						TypeReference.of(OAuth2AuthenticationToken.class),
-						TypeReference.of(DefaultOidcUser.class),
-						TypeReference.of(DefaultOAuth2User.class),
-						TypeReference.of(OidcUserAuthority.class),
-						TypeReference.of(OAuth2UserAuthority.class),
-						TypeReference.of(SimpleGrantedAuthority.class),
-						TypeReference.of(OidcIdToken.class),
-						TypeReference.of(AbstractOAuth2Token.class),
-						TypeReference.of(OidcUserInfo.class),
-						TypeReference.of(OAuth2AuthorizationRequest.class),
-						TypeReference.of(AuthorizationGrantType.class),
-						TypeReference.of(OAuth2AuthorizationResponseType.class),
-						TypeReference.of(OAuth2TokenFormat.class)
-				), builder ->
-						builder.withMembers(MemberCategory.DECLARED_FIELDS,
-								MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS)
-		);
+		if (ClassUtils.isPresent("org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken",
+				ClassUtils.getDefaultClassLoader()))
+			hints.reflection().registerType(
+					TypeReference
+							.of("org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken"),
+					builder -> builder.withMembers(MemberCategory.DECLARED_FIELDS,
+							MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
+
+		hints.reflection().registerTypes(Arrays.asList(TypeReference.of(AbstractAuthenticationToken.class),
+				TypeReference.of(WebAuthenticationDetails.class),
+				TypeReference.of(UsernamePasswordAuthenticationToken.class), TypeReference.of(User.class),
+				TypeReference.of(DefaultOidcUser.class), TypeReference.of(DefaultOAuth2User.class),
+				TypeReference.of(OidcUserAuthority.class), TypeReference.of(OAuth2UserAuthority.class),
+				TypeReference.of(SimpleGrantedAuthority.class), TypeReference.of(OidcIdToken.class),
+				TypeReference.of(AbstractOAuth2Token.class), TypeReference.of(OidcUserInfo.class),
+				TypeReference.of(OAuth2AuthorizationRequest.class), TypeReference.of(AuthorizationGrantType.class),
+				TypeReference.of(OAuth2AuthorizationResponseType.class), TypeReference.of(OAuth2TokenFormat.class)),
+				builder -> builder.withMembers(MemberCategory.DECLARED_FIELDS,
+						MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
 
 		// Jackson Modules - Spring Security and Spring Authorization Server
 		hints.reflection().registerTypes(
-				Arrays.asList(
-						TypeReference.of(CoreJackson2Module.class),
-						TypeReference.of(WebServletJackson2Module.class),
-						TypeReference.of(OAuth2ClientJackson2Module.class),
-						TypeReference.of(OAuth2AuthorizationServerJackson2Module.class)
-				), builder ->
-						builder.withMembers(MemberCategory.DECLARED_FIELDS,
-								MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS)
-		);
+				Set.of(CoreJackson2Module.class.getName(), WebServletJackson2Module.class.getName(),
+						OAuth2AuthorizationServerJackson2Module.class.getName()).stream().map(TypeReference::of)
+						.toList(),
+				builder -> builder.withMembers(MemberCategory.DECLARED_FIELDS,
+						MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
+
+		if (ClassUtils.isPresent("org.springframework.security.oauth2.client.jackson2.OAuth2ClientJackson2Module",
+				ClassUtils.getDefaultClassLoader()))
+			hints.reflection().registerType(
+					TypeReference.of("org.springframework.security.oauth2.client.jackson2.OAuth2ClientJackson2Module"),
+					b -> b.withMembers(MemberCategory.DECLARED_FIELDS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+							MemberCategory.INVOKE_DECLARED_METHODS));
+
+		hints.reflection().registerTypes(
+				Set.of(CoreJackson2Module.class.getName(), WebServletJackson2Module.class.getName(),
+						OAuth2AuthorizationServerJackson2Module.class.getName()).stream().map(TypeReference::of)
+						.toList(),
+				builder -> builder.withMembers(MemberCategory.DECLARED_FIELDS,
+						MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
 
 		// Jackson Mixins - Spring Security and Spring Authorization Server
 		try {
@@ -126,8 +116,8 @@ class DemoAuthorizationServerRuntimeHints implements RuntimeHintsRegistrar {
 					Class.forName("org.springframework.security.jackson2.UnmodifiableListMixin"));
 			this.reflectionHintsRegistrar.registerReflectionHints(hints.reflection(),
 					Class.forName("org.springframework.security.jackson2.UnmodifiableMapMixin"));
-			this.reflectionHintsRegistrar.registerReflectionHints(hints.reflection(),
-					Class.forName("org.springframework.security.oauth2.server.authorization.jackson2.UnmodifiableMapMixin"));
+			this.reflectionHintsRegistrar.registerReflectionHints(hints.reflection(), Class
+					.forName("org.springframework.security.oauth2.server.authorization.jackson2.UnmodifiableMapMixin"));
 			this.reflectionHintsRegistrar.registerReflectionHints(hints.reflection(),
 					Class.forName("org.springframework.security.oauth2.server.authorization.jackson2.HashSetMixin"));
 			this.reflectionHintsRegistrar.registerReflectionHints(hints.reflection(),
@@ -136,8 +126,8 @@ class DemoAuthorizationServerRuntimeHints implements RuntimeHintsRegistrar {
 					Class.forName("org.springframework.security.jackson2.UsernamePasswordAuthenticationTokenMixin"));
 			this.reflectionHintsRegistrar.registerReflectionHints(hints.reflection(),
 					Class.forName("org.springframework.security.jackson2.UserMixin"));
-			this.reflectionHintsRegistrar.registerReflectionHints(hints.reflection(),
-					Class.forName("org.springframework.security.oauth2.client.jackson2.OAuth2AuthenticationTokenMixin"));
+			this.reflectionHintsRegistrar.registerReflectionHints(hints.reflection(), Class
+					.forName("org.springframework.security.oauth2.client.jackson2.OAuth2AuthenticationTokenMixin"));
 			this.reflectionHintsRegistrar.registerReflectionHints(hints.reflection(),
 					Class.forName("org.springframework.security.oauth2.client.jackson2.DefaultOidcUserMixin"));
 			this.reflectionHintsRegistrar.registerReflectionHints(hints.reflection(),
@@ -152,21 +142,15 @@ class DemoAuthorizationServerRuntimeHints implements RuntimeHintsRegistrar {
 					Class.forName("org.springframework.security.oauth2.client.jackson2.OidcIdTokenMixin"));
 			this.reflectionHintsRegistrar.registerReflectionHints(hints.reflection(),
 					Class.forName("org.springframework.security.oauth2.client.jackson2.OidcUserInfoMixin"));
-			this.reflectionHintsRegistrar.registerReflectionHints(hints.reflection(),
-					Class.forName("org.springframework.security.oauth2.server.authorization.jackson2.OAuth2AuthorizationRequestMixin"));
-			this.reflectionHintsRegistrar.registerReflectionHints(hints.reflection(),
-					Class.forName("org.springframework.security.oauth2.server.authorization.jackson2.OAuth2TokenFormatMixin"));
-		} catch (ClassNotFoundException ex) {
+			this.reflectionHintsRegistrar.registerReflectionHints(hints.reflection(), Class.forName(
+					"org.springframework.security.oauth2.server.authorization.jackson2.OAuth2AuthorizationRequestMixin"));
+			this.reflectionHintsRegistrar.registerReflectionHints(hints.reflection(), Class.forName(
+					"org.springframework.security.oauth2.server.authorization.jackson2.OAuth2TokenFormatMixin"));
+		}
+		catch (ClassNotFoundException ex) {
 			throw new RuntimeException(ex);
 		}
 
-		// Sql Schema Resources
-		hints.resources().registerPattern(
-				"org/springframework/security/oauth2/server/authorization/client/oauth2-registered-client-schema.sql");
-		hints.resources().registerPattern(
-				"org/springframework/security/oauth2/server/authorization/oauth2-authorization-schema.sql");
-		hints.resources().registerPattern(
-				"org/springframework/security/oauth2/server/authorization/oauth2-authorization-consent-schema.sql");
 	}
 
 }
--- oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/server/authorization/JdbcOAuth2AuthorizationConsentService.java
+++ oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/server/authorization/JdbcOAuth2AuthorizationConsentService.java
@@ -24,6 +24,10 @@ import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
 
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.dao.DataRetrievalFailureException;
 import org.springframework.jdbc.core.ArgumentPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcOperations;
@@ -43,19 +47,31 @@ import org.springframework.util.StringUtils;
  * {@link JdbcOperations} for {@link OAuth2AuthorizationConsent} persistence.
  *
  * <p>
- * <b>NOTE:</b> This {@code OAuth2AuthorizationConsentService} depends on the table definition
- * described in
- * "classpath:org/springframework/security/oauth2/server/authorization/oauth2-authorization-consent-schema.sql" and
- * therefore MUST be defined in the database schema.
+ * <b>NOTE:</b> This {@code OAuth2AuthorizationConsentService} depends on the table
+ * definition described in
+ * "classpath:org/springframework/security/oauth2/server/authorization/oauth2-authorization-consent-schema.sql"
+ * and therefore MUST be defined in the database schema.
  *
  * @author Ovidiu Popa
+ * @author Josh Long
  * @since 0.1.2
  * @see OAuth2AuthorizationConsentService
  * @see OAuth2AuthorizationConsent
  * @see JdbcOperations
  * @see RowMapper
  */
+@ImportRuntimeHints(JdbcOAuth2AuthorizationConsentService.JdbcOAuth2AuthorizationConsentServiceRuntimeHintsRegistrar.class)
 public class JdbcOAuth2AuthorizationConsentService implements OAuth2AuthorizationConsentService {
+
+	static class JdbcOAuth2AuthorizationConsentServiceRuntimeHintsRegistrar implements RuntimeHintsRegistrar {
+
+		@Override
+		public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+			hints.resources().registerResource(new ClassPathResource(
+					"org/springframework/security/oauth2/server/authorization/oauth2-authorization-consent-schema.sql"));
+		}
+
+	}
 
 	// @formatter:off
 	private static final String COLUMN_NAMES = "registered_client_id, "
@@ -87,13 +103,15 @@ public class JdbcOAuth2AuthorizationConsentService implements OAuth2Authorizatio
 	private static final String REMOVE_AUTHORIZATION_CONSENT_SQL = "DELETE FROM " + TABLE_NAME + " WHERE " + PK_FILTER;
 
 	private final JdbcOperations jdbcOperations;
+
 	private RowMapper<OAuth2AuthorizationConsent> authorizationConsentRowMapper;
+
 	private Function<OAuth2AuthorizationConsent, List<SqlParameterValue>> authorizationConsentParametersMapper;
 
 	/**
-	 * Constructs a {@code JdbcOAuth2AuthorizationConsentService} using the provided parameters.
-	 *
-	 * @param jdbcOperations             the JDBC operations
+	 * Constructs a {@code JdbcOAuth2AuthorizationConsentService} using the provided
+	 * parameters.
+	 * @param jdbcOperations the JDBC operations
 	 * @param registeredClientRepository the registered client repository
 	 */
 	public JdbcOAuth2AuthorizationConsentService(JdbcOperations jdbcOperations,
@@ -108,11 +126,12 @@ public class JdbcOAuth2AuthorizationConsentService implements OAuth2Authorizatio
 	@Override
 	public void save(OAuth2AuthorizationConsent authorizationConsent) {
 		Assert.notNull(authorizationConsent, "authorizationConsent cannot be null");
-		OAuth2AuthorizationConsent existingAuthorizationConsent = findById(
-				authorizationConsent.getRegisteredClientId(), authorizationConsent.getPrincipalName());
+		OAuth2AuthorizationConsent existingAuthorizationConsent = findById(authorizationConsent.getRegisteredClientId(),
+				authorizationConsent.getPrincipalName());
 		if (existingAuthorizationConsent == null) {
 			insertAuthorizationConsent(authorizationConsent);
-		} else {
+		}
+		else {
 			updateAuthorizationConsent(authorizationConsent);
 		}
 	}
@@ -138,8 +157,7 @@ public class JdbcOAuth2AuthorizationConsentService implements OAuth2Authorizatio
 		Assert.notNull(authorizationConsent, "authorizationConsent cannot be null");
 		SqlParameterValue[] parameters = new SqlParameterValue[] {
 				new SqlParameterValue(Types.VARCHAR, authorizationConsent.getRegisteredClientId()),
-				new SqlParameterValue(Types.VARCHAR, authorizationConsent.getPrincipalName())
-		};
+				new SqlParameterValue(Types.VARCHAR, authorizationConsent.getPrincipalName()) };
 		PreparedStatementSetter pss = new ArgumentPreparedStatementSetter(parameters);
 		this.jdbcOperations.update(REMOVE_AUTHORIZATION_CONSENT_SQL, pss);
 	}
@@ -151,7 +169,7 @@ public class JdbcOAuth2AuthorizationConsentService implements OAuth2Authorizatio
 		Assert.hasText(principalName, "principalName cannot be empty");
 		SqlParameterValue[] parameters = new SqlParameterValue[] {
 				new SqlParameterValue(Types.VARCHAR, registeredClientId),
-				new SqlParameterValue(Types.VARCHAR, principalName)};
+				new SqlParameterValue(Types.VARCHAR, principalName) };
 		PreparedStatementSetter pss = new ArgumentPreparedStatementSetter(parameters);
 		List<OAuth2AuthorizationConsent> result = this.jdbcOperations.query(LOAD_AUTHORIZATION_CONSENT_SQL, pss,
 				this.authorizationConsentRowMapper);
@@ -162,22 +180,21 @@ public class JdbcOAuth2AuthorizationConsentService implements OAuth2Authorizatio
 	 * Sets the {@link RowMapper} used for mapping the current row in
 	 * {@code java.sql.ResultSet} to {@link OAuth2AuthorizationConsent}. The default is
 	 * {@link OAuth2AuthorizationConsentRowMapper}.
-	 *
-	 * @param authorizationConsentRowMapper the {@link RowMapper} used for mapping the current
-	 *                                      row in {@code ResultSet} to {@link OAuth2AuthorizationConsent}
+	 * @param authorizationConsentRowMapper the {@link RowMapper} used for mapping the
+	 * current row in {@code ResultSet} to {@link OAuth2AuthorizationConsent}
 	 */
-	public final void setAuthorizationConsentRowMapper(RowMapper<OAuth2AuthorizationConsent> authorizationConsentRowMapper) {
+	public final void setAuthorizationConsentRowMapper(
+			RowMapper<OAuth2AuthorizationConsent> authorizationConsentRowMapper) {
 		Assert.notNull(authorizationConsentRowMapper, "authorizationConsentRowMapper cannot be null");
 		this.authorizationConsentRowMapper = authorizationConsentRowMapper;
 	}
 
 	/**
-	 * Sets the {@code Function} used for mapping {@link OAuth2AuthorizationConsent} to
-	 * a {@code List} of {@link SqlParameterValue}. The default is
+	 * Sets the {@code Function} used for mapping {@link OAuth2AuthorizationConsent} to a
+	 * {@code List} of {@link SqlParameterValue}. The default is
 	 * {@link OAuth2AuthorizationConsentParametersMapper}.
-	 *
 	 * @param authorizationConsentParametersMapper the {@code Function} used for mapping
-	 *                                             {@link OAuth2AuthorizationConsent} to a {@code List} of {@link SqlParameterValue}
+	 * {@link OAuth2AuthorizationConsent} to a {@code List} of {@link SqlParameterValue}
 	 */
 	public final void setAuthorizationConsentParametersMapper(
 			Function<OAuth2AuthorizationConsent, List<SqlParameterValue>> authorizationConsentParametersMapper) {
@@ -198,10 +215,11 @@ public class JdbcOAuth2AuthorizationConsentService implements OAuth2Authorizatio
 	}
 
 	/**
-	 * The default {@link RowMapper} that maps the current row in
-	 * {@code ResultSet} to {@link OAuth2AuthorizationConsent}.
+	 * The default {@link RowMapper} that maps the current row in {@code ResultSet} to
+	 * {@link OAuth2AuthorizationConsent}.
 	 */
 	public static class OAuth2AuthorizationConsentRowMapper implements RowMapper<OAuth2AuthorizationConsent> {
+
 		private final RegisteredClientRepository registeredClientRepository;
 
 		public OAuth2AuthorizationConsentRowMapper(RegisteredClientRepository registeredClientRepository) {
@@ -214,13 +232,14 @@ public class JdbcOAuth2AuthorizationConsentService implements OAuth2Authorizatio
 			String registeredClientId = rs.getString("registered_client_id");
 			RegisteredClient registeredClient = this.registeredClientRepository.findById(registeredClientId);
 			if (registeredClient == null) {
-				throw new DataRetrievalFailureException(
-						"The RegisteredClient with id '" + registeredClientId + "' was not found in the RegisteredClientRepository.");
+				throw new DataRetrievalFailureException("The RegisteredClient with id '" + registeredClientId
+						+ "' was not found in the RegisteredClientRepository.");
 			}
 
 			String principalName = rs.getString("principal_name");
 
-			OAuth2AuthorizationConsent.Builder builder = OAuth2AuthorizationConsent.withId(registeredClientId, principalName);
+			OAuth2AuthorizationConsent.Builder builder = OAuth2AuthorizationConsent.withId(registeredClientId,
+					principalName);
 			String authorizationConsentAuthorities = rs.getString("authorities");
 			if (authorizationConsentAuthorities != null) {
 				for (String authority : StringUtils.commaDelimitedListToSet(authorizationConsentAuthorities)) {
@@ -240,7 +259,8 @@ public class JdbcOAuth2AuthorizationConsentService implements OAuth2Authorizatio
 	 * The default {@code Function} that maps {@link OAuth2AuthorizationConsent} to a
 	 * {@code List} of {@link SqlParameterValue}.
 	 */
-	public static class OAuth2AuthorizationConsentParametersMapper implements Function<OAuth2AuthorizationConsent, List<SqlParameterValue>> {
+	public static class OAuth2AuthorizationConsentParametersMapper
+			implements Function<OAuth2AuthorizationConsent, List<SqlParameterValue>> {
 
 		@Override
 		public List<SqlParameterValue> apply(OAuth2AuthorizationConsent authorizationConsent) {
@@ -252,7 +272,8 @@ public class JdbcOAuth2AuthorizationConsentService implements OAuth2Authorizatio
 			for (GrantedAuthority authority : authorizationConsent.getAuthorities()) {
 				authorities.add(authority.getAuthority());
 			}
-			parameters.add(new SqlParameterValue(Types.VARCHAR, StringUtils.collectionToDelimitedString(authorities, ",")));
+			parameters.add(
+					new SqlParameterValue(Types.VARCHAR, StringUtils.collectionToDelimitedString(authorities, ",")));
 			return parameters;
 		}
 

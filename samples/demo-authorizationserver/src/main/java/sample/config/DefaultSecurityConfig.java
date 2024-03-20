36ac1138111b6386812657e3e1e25cb5c2f9d08a
김지훈 <80799163+xxxjjhhh@users.noreply.github.com>
2024-03-20 10:26:46 +0900

    Update DefaultSecurityConfig.java

--- samples/demo-authorizationserver/src/main/java/sample/config/DefaultSecurityConfig.java
+++ samples/demo-authorizationserver/src/main/java/sample/config/DefaultSecurityConfig.java
@@ -26,6 +26,7 @@ import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;

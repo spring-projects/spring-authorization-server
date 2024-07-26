48115faee730fa020b1e5a6481ed4fa49d21a2c0
aijaz2 <aijaz.keerio@sky.uk>
2024-07-26 17:50:31 +0100

    Fix empty code parameter in CodeVerifierAuthenticator
    Closes gh-1680

--- oauth2-authorization-server/src/test/java/org/springframework/security/oauth2/server/authorization/authentication/ClientSecretAuthenticationProviderTests.java
+++ oauth2-authorization-server/src/test/java/org/springframework/security/oauth2/server/authorization/authentication/ClientSecretAuthenticationProviderTests.java
@@ -46,6 +46,7 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 /**

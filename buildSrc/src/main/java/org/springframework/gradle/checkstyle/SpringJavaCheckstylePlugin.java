1255b65e65727cc926b745961d8c91e39d825335
Joe Grandja <10884212+jgrandja@users.noreply.github.com>
2024-10-10 10:58:33 -0400

    Update to nohttp-gradle 0.0.11
    Closes gh-1733

--- buildSrc/src/main/java/org/springframework/gradle/checkstyle/SpringJavaCheckstylePlugin.java
+++ buildSrc/src/main/java/org/springframework/gradle/checkstyle/SpringJavaCheckstylePlugin.java
@@ -39,7 +39,7 @@ public class SpringJavaCheckstylePlugin implements Plugin<Project> {
 	private static final String SPRING_JAVAFORMAT_VERSION_PROPERTY = "springJavaformatVersion";
 	private static final String DEFAULT_SPRING_JAVAFORMAT_VERSION = "0.0.41";
 	private static final String NOHTTP_CHECKSTYLE_VERSION_PROPERTY = "nohttpCheckstyleVersion";
-	private static final String DEFAULT_NOHTTP_CHECKSTYLE_VERSION = "0.0.10";
+	private static final String DEFAULT_NOHTTP_CHECKSTYLE_VERSION = "0.0.11";
 	private static final String CHECKSTYLE_TOOL_VERSION_PROPERTY = "checkstyleToolVersion";
 	private static final String DEFAULT_CHECKSTYLE_TOOL_VERSION = "8.34";
 	private static final String SPRING_JAVAFORMAT_EXCLUDE_PACKAGES_PROPERTY = "springJavaformatExcludePackages";
